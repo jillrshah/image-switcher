b84a25dde92483683b85ab6162a2b4fa42dd4427
Jill Shah <jillrshah@gmail.com>
2020-03-03 11:59:47 -0500

    DRY code

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-$("body").on("click", "#man", function (e) {
+$("body").on("click", ".my-button", function (e) {
 	e.preventDefault();
-	const url = $("#man").data("image");
-	console.log(url);
-	$("img").attr("src", url);
 
-});
-$("body").on("click", "#baby", function (e) {
-	e.preventDefault();
-	const url = $("#baby").data("image");
+	const this_button = e.target;
+
+	const url = $(this_button).data("image");
+
 	console.log(url);
 	$("img").attr("src", url);
 
